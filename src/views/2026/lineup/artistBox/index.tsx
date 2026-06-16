30ee227e199fa54686e9f27da165137455849a5d
TakedaRyota <tkdryt05@gmail.com>
2026-06-16 15:23:56 +0900

    safari bug

--- src/views/2026/lineup/artistBox/index.tsx
+++ src/views/2026/lineup/artistBox/index.tsx
@@ -9,9 +9,9 @@ export const ArtistBox = (props: ArtistType) => {
   return (
     <SlideUpAnimation>
       <div className='flex flex-col p-3 bg-white rounded-sm border border-sekigahara custom-shadow artist-box w-full'>
-        <div className='flex flex-co w-full text-center justify-center overflow-hidden relative lg:h-[250px] md:h-[230px] sm:h-[200px] min-[150px]:h-[100px]'>
+        <div className='flex flex-co w-full text-center justify-center overflow-hidden relative lg:h-[250px] md:h-[230px] sm:h-[200px] min-[150px]:h-[200px]'>
           {/* <Img src={img.src} alt={img.alt} cName='h-full absolute top-0 left-0 object-contain' /> */}
-          <Img src={img.src} alt={img.alt} cName='h-full absolute top-0 left-0 scale-150 sm:scale-140 translate-y-2 sm:translate-y-4' />
+          <Img src={img.src} alt={img.alt} cName='h-[200%] absolute top-0 left-0 -translate-y-9 sm:-translate-y-11' />
         </div>
         <div className='text-sekigahara text-md md:text-xl font-bold text-center h-14'>
           <p className='whitespace-pre-wrap h-full content-center leading-5'>{name}</p>
